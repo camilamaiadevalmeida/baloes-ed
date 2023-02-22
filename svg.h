0b3fc4a3fb862c2539e65eca1cb49c95ce0d2809
tonhaomototaxi <vaniapolo@gmail.com>
2023-02-22 00:03:58 -0300

    svg

--- svg.h
+++ svg.h
@@ -1,7 +1,6 @@
 #ifndef ARQSVG__
 #define ARQSVG__
 
-
 /*
   Provê operações para escrita de arquivos básicos no formato SVG.
 
@@ -29,12 +28,11 @@ typedef void *ArqSvg;
  */
 ArqSvg abreEscritaSvg(char *fn);
 
-
 /*
   Preenche o vetor "deco" (com capacidade de "decoLen" caracteres) com atributos
 para formatacao de uma figura SVG. Este vetor pode, posteriormente, ser usado
 como argumento em invocacoes de escreveCirculoSvg, escreveRetanguloSvg,
-escreveLinhaSvg. Para não incluir algum dos parâmetros na decoracao, 
+escreveLinhaSvg. Para não incluir algum dos parâmetros na decoracao,
 atribuir NULL (para char *) ou valores negativos (double) ao respectivo parametro.
 
     Nao considerar valor: NULL para char * e valores negativos para double
@@ -43,34 +41,33 @@ atribuir NULL (para char *) ou valores negativos (double) ao respectivo parametr
     text anchor: start | middle |end
  */
 void preparaDecoracao(ArqSvg fsvg, char *deco, int decoLen,
-		      char *corBorda, char *corPreenchimento,
-		      char *larguraBorda, double transparencia,
-		      double transparenciaPreenchimento, double transparenciaBorda);
+                      char *corBorda, char *corPreenchimento,
+                      char *larguraBorda, double transparencia,
+                      double transparenciaPreenchimento, double transparenciaBorda);
 
 /*
    Insere uma circunferencia com centro em (xc,yc) de raio r. Atributos de formatacao
-(cores, largura de bordas, etc) podem ser informados pela string deco (atribuir NULL, 
+(cores, largura de bordas, etc) podem ser informados pela string deco (atribuir NULL,
 caso nao queira atribuir decoracao). As formatacoes mais usuais podem preparadas por
 meio da operacao preparaDecoracao.
  */
 void escreveCirculoSvg(ArqSvg fsvg, double xc, double yc, double r, char *deco);
 
 /*
-   Insere um retangulo com canto inferior esquerdo  em (x,y), de largura larg e altura alt. 
-Atributos de formatacao (cores, largura de bordas, etc) podem ser informados pela string 
-deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais 
+   Insere um retangulo com canto inferior esquerdo  em (x,y), de largura larg e altura alt.
+Atributos de formatacao (cores, largura de bordas, etc) podem ser informados pela string
+deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais
 podem preparadas por meio da operacao preparaDecoracao.
  */
 void escreveRetanguloSvg(ArqSvg fsvg, double x, double y, double larg, double alt, char *deco);
 
 /*
-   Insere uma linha com extremidades em (x1,y1) e (x2,y2). 
-Atributos de formatacao (cores, largura de bordas, etc) podem ser informados pela string 
-deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais 
+   Insere uma linha com extremidades em (x1,y1) e (x2,y2).
+Atributos de formatacao (cores, largura de bordas, etc) podem ser informados pela string
+deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais
 podem preparadas por meio da operacao preparaDecoracao.
  */
 void escreveLinhaSvg(ArqSvg fsvg, double x1, double y1, double x2, double y2, char *deco);
-
 
 /* Font style: */
 const char *FS_NORMAL = "normal";
@@ -88,31 +85,30 @@ const char *TA_START = "start";
 const char *TA_MIDDLE = "middle";
 const char *TA_END = "end";
 
-
 /*
   Preenche o vetor "deco" (com capacidade de "decoLen" caracteres) com atributos
 para formatacao de um texto SVG. Este vetor pode, posteriormente, ser usado
 como argumento em invocacoes de escreveTextoSvg.
-Para não incluir algum dos parâmetros na decoracao, 
+Para não incluir algum dos parâmetros na decoracao,
 atribuir NULL (para char *) ao respectivo parametro.
 
   Os valores para os parametros estao relacionados abaixo. Usar as constantes
 acima.
-   
+
     font style: normal | italic | oblique
     font weight: normal | bold | bolder | lighter
     text anchor: start | middle |end
  */
 
 void preparaDecoracaoTexto(ArqSvg fsvg, char *deco, int decoLen,
-			   char *fontFamily, char *fontStyle,
-			   char *fontWeight, char *fontSize,
-			   char *fontColor, char *textAnchor);
+                           char *fontFamily, char *fontStyle,
+                           char *fontWeight, char *fontSize,
+                           char *fontColor, char *textAnchor);
 
 /*
   Insere no arquivo svg o texto txt na coordenada (x,y).
-Atributos de formatacao (cores, fonte, etc) podem ser informados pela string 
-deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais 
+Atributos de formatacao (cores, fonte, etc) podem ser informados pela string
+deco (atribuir NULL, caso nao queira atribuir decoracao). As formatacoes mais usuais
 podem preparadas por meio da operacao preparaDecoracaoTexto.
  */
 void escreveTextoSvg(ArqSvg fsvg, double x, double y, char *txt, char *decoTxt);
